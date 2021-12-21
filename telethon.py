7c57b26ad13cdb16063d8bcf868cf3eea3185db2
monsterZD-git <oren_rebel@mail.ru>
2021-12-21 20:57:16 +0300

    Create telethon.py

---
+++ telethon.py
@@ -0,0 +1,17 @@
+from telethon import TelegramClient, sync, events
+import json
+import requests
+
+# Вставляем api_id и api_hash
+api_id = ''
+api_hash = ''
+
+client = TelegramClient('session_name', api_id, api_hash)
+
+@client.on(events.NewMessage(from_users=['USDTEMA4_bot']))
+async def indicatum_handler(event):
+    message = event.message.message
+    print(message)
+
+client.start()
+client.run_until_disconnected()
